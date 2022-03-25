3a168e1a05f19f5df7b287179a79de7bc2d38450
Moctar Diallo <moctarjallo@gmail.com>
2022-03-25 20:34:16 +0000

    Added train_network function

--- test_network.py
+++ test_network.py
@@ -1,5 +1,5 @@
 from random import seed
-from network import initialize_network, forward_propagate, backward_propagate_error
+from network import initialize_network, forward_propagate, backward_propagate_error, train_network
 
 import unittest
 
@@ -26,6 +26,27 @@ class TestNetwork(unittest.TestCase):
                 [{'output': 0.6213859615555266, 'weights': [0.2550690257394217, 0.49543508709194095]}, {'output': 0.6573693455986976, 'weights': [0.4494910647887381, 0.651592972722763]}]]
         expected = [0, 1]
         backward_propagate_error(network, expected)
+        for layer in network:
+            print(layer)
+
+    def test_train_network(self):
+        # Test training backprop algorithm
+        print("Testing train_network:")
+        seed(1)
+        dataset = [[2.7810836,2.550537003,0],
+            [1.465489372,2.362125076,0],
+            [3.396561688,4.400293529,0],
+            [1.38807019,1.850220317,0],
+            [3.06407232,3.005305973,0],
+            [7.627531214,2.759262235,1],
+            [5.332441248,2.088626775,1],
+            [6.922596716,1.77106367,1],
+            [8.675418651,-0.242068655,1],
+            [7.673756466,3.508563011,1]]
+        n_inputs = len(dataset[0]) - 1
+        n_outputs = len(set([row[-1] for row in dataset]))
+        network = initialize_network(n_inputs, 2, n_outputs)
+        train_network(network, dataset, 0.5, 20, n_outputs)
         for layer in network:
             print(layer)
 
