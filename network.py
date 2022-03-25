d3db1ab41d87fc6bfefcfd7f745782adf51162f8
Moctar Diallo <moctarjallo@gmail.com>
2022-03-25 20:16:08 +0000

    Added activation, transfer and forward_propagation functions

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 from random import random
+from math import exp 
 
 # Initialize a network
 def initialize_network(n_inputs, n_hidden, n_outputs):
@@ -8,3 +9,28 @@ def initialize_network(n_inputs, n_hidden, n_outputs):
 	output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
 	network.append(output_layer)
 	return network
+
+
+# Calculate neuron activation for an input
+def activate(weights, inputs):
+	activation = weights[-1]
+	for i in range(len(weights)-1):
+		activation += weights[i] * inputs[i]
+	return activation
+
+# Transfer neuron activation
+def transfer(activation):
+	return 1.0 / (1.0 + exp(-activation))
+
+
+# Forward propagate input to a network output
+def forward_propagate(network, row):
+	inputs = row
+	for layer in network:
+		new_inputs = []
+		for neuron in layer:
+			activation = activate(neuron['weights'], inputs)
+			neuron['output'] = transfer(activation)
+			new_inputs.append(neuron['output'])
+		inputs = new_inputs
+	return inputs
--- test_network.py
+++ test_network.py
@@ -1,5 +1,5 @@
 from random import seed
-from network import initialize_network
+from network import initialize_network, forward_propagate
 
 import unittest
 
@@ -8,8 +8,16 @@ class TestNetwork(unittest.TestCase):
         seed(1)
         network = initialize_network(2, 1, 2)
         for layer in network:
-            print(layer)
+            print('layer:', layer)
 
+    def test_forward_propagate(self):
+        # test forward propagation
+        network = [[{'weights': [0.13436424411240122, 0.8474337369372327, 0.763774618976614]}],
+                [{'weights': [0.2550690257394217, 0.49543508709194095]}, {'weights': [0.4494910647887381, 0.651592972722763]}]]
+        row = [1, 0, None]
+        output = forward_propagate(network, row)
+        print()
+        print('output:', output)
 
 if __name__ == '__main__':
     unittest.main()
