3c8cc9e78cd00ef61cce8768a8db60fcb1a5833d
Moctar Diallo <moctarjallo@gmail.com>
2022-03-27 03:45:52 +0000

    Applied back_propagation algorithm to real seeds data

--- network.py
+++ network.py
@@ -1,15 +1,88 @@
+# Backprop on the Seeds Dataset
+from random import seed
+from random import randrange
 from random import random
-from math import exp 
+from csv import reader
+from math import exp
 
-# Initialize a network
-def initialize_network(n_inputs, n_hidden, n_outputs):
-	network = list()
-	hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
-	network.append(hidden_layer)
-	output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
-	network.append(output_layer)
-	return network
+# Load a CSV file
+def load_csv(filename):
+	dataset = list()
+	with open(filename, 'r') as file:
+		csv_reader = reader(file)
+		for row in csv_reader:
+			if not row:
+				continue
+			dataset.append(row)
+	return dataset
 
+# Convert string column to float
+def str_column_to_float(dataset, column):
+	for row in dataset:
+		row[column] = float(row[column].strip())
+
+# Convert string column to integer
+def str_column_to_int(dataset, column):
+	class_values = [row[column] for row in dataset]
+	unique = set(class_values)
+	lookup = dict()
+	for i, value in enumerate(unique):
+		lookup[value] = i
+	for row in dataset:
+		row[column] = lookup[row[column]]
+	return lookup
+
+# Find the min and max values for each column
+def dataset_minmax(dataset):
+	minmax = list()
+	stats = [[min(column), max(column)] for column in zip(*dataset)]
+	return stats
+
+# Rescale dataset columns to the range 0-1
+def normalize_dataset(dataset, minmax):
+	for row in dataset:
+		for i in range(len(row)-1):
+			row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
+
+# Split a dataset into k folds
+def cross_validation_split(dataset, n_folds):
+	dataset_split = list()
+	dataset_copy = list(dataset)
+	fold_size = int(len(dataset) / n_folds)
+	for i in range(n_folds):
+		fold = list()
+		while len(fold) < fold_size:
+			index = randrange(len(dataset_copy))
+			fold.append(dataset_copy.pop(index))
+		dataset_split.append(fold)
+	return dataset_split
+
+# Calculate accuracy percentage
+def accuracy_metric(actual, predicted):
+	correct = 0
+	for i in range(len(actual)):
+		if actual[i] == predicted[i]:
+			correct += 1
+	return correct / float(len(actual)) * 100.0
+
+# Evaluate an algorithm using a cross validation split
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+	folds = cross_validation_split(dataset, n_folds)
+	scores = list()
+	for fold in folds:
+		train_set = list(folds)
+		train_set.remove(fold)
+		train_set = sum(train_set, [])
+		test_set = list()
+		for row in fold:
+			row_copy = list(row)
+			test_set.append(row_copy)
+			row_copy[-1] = None
+		predicted = algorithm(train_set, test_set, *args)
+		actual = [row[-1] for row in fold]
+		accuracy = accuracy_metric(actual, predicted)
+		scores.append(accuracy)
+	return scores
 
 # Calculate neuron activation for an input
 def activate(weights, inputs):
@@ -21,7 +94,6 @@ def activate(weights, inputs):
 # Transfer neuron activation
 def transfer(activation):
 	return 1.0 / (1.0 + exp(-activation))
-
 
 # Forward propagate input to a network output
 def forward_propagate(network, row):
@@ -35,11 +107,9 @@ def forward_propagate(network, row):
 		inputs = new_inputs
 	return inputs
 
-
 # Calculate the derivative of an neuron output
 def transfer_derivative(output):
 	return output * (1.0 - output)
-
 
 # Backpropagate error and store in neurons
 def backward_propagate_error(network, expected):
@@ -60,7 +130,6 @@ def backward_propagate_error(network, expected):
 			neuron = layer[j]
 			neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
 
-
 # Update network weights with error
 def update_weights(network, row, l_rate):
 	for i in range(len(network)):
@@ -72,21 +141,38 @@ def update_weights(network, row, l_rate):
 				neuron['weights'][j] -= l_rate * neuron['delta'] * inputs[j]
 			neuron['weights'][-1] -= l_rate * neuron['delta']
 
-
 # Train a network for a fixed number of epochs
 def train_network(network, train, l_rate, n_epoch, n_outputs):
 	for epoch in range(n_epoch):
-		sum_error = 0
 		for row in train:
 			outputs = forward_propagate(network, row)
 			expected = [0 for i in range(n_outputs)]
 			expected[row[-1]] = 1
-			sum_error += sum([(expected[i]-outputs[i])**2 for i in range(len(expected))])
 			backward_propagate_error(network, expected)
 			update_weights(network, row, l_rate)
-		print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+
+# Initialize a network
+def initialize_network(n_inputs, n_hidden, n_outputs):
+	network = list()
+	hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+	network.append(hidden_layer)
+	output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+	network.append(output_layer)
+	return network
 
 # Make a prediction with a network
 def predict(network, row):
 	outputs = forward_propagate(network, row)
 	return outputs.index(max(outputs))
+
+# Backpropagation Algorithm With Stochastic Gradient Descent
+def back_propagation(train, test, l_rate, n_epoch, n_hidden):
+	n_inputs = len(train[0]) - 1
+	n_outputs = len(set([row[-1] for row in train]))
+	network = initialize_network(n_inputs, n_hidden, n_outputs)
+	train_network(network, train, l_rate, n_epoch, n_outputs)
+	predictions = list()
+	for row in test:
+		prediction = predict(network, row)
+		predictions.append(prediction)
+	return(predictions)
--- test_network.py
+++ test_network.py
@@ -1,5 +1,6 @@
 from random import seed
 from network import initialize_network, forward_propagate, backward_propagate_error, train_network, predict
+from network import load_csv, str_column_to_float, str_column_to_int, dataset_minmax, normalize_dataset, back_propagation, evaluate_algorithm
 
 import unittest
 
@@ -68,6 +69,29 @@ class TestNetwork(unittest.TestCase):
         for row in dataset:
             prediction = predict(network, row)
             print('Expected=%d, Got=%d' % (row[-1], prediction))
+
+    def test_back_propagation(self):
+        # Test Backprop on Seeds dataset
+        print("Testing back_propagation algorithm:")
+        seed(1)
+        # load and prepare data
+        filename = 'seeds_dataset.csv'
+        dataset = load_csv(filename)
+        for i in range(len(dataset[0])-1):
+            str_column_to_float(dataset, i)
+        # convert class column to integers
+        str_column_to_int(dataset, len(dataset[0])-1)
+        # normalize input variables
+        minmax = dataset_minmax(dataset)
+        normalize_dataset(dataset, minmax)
+        # evaluate algorithm
+        n_folds = 5
+        l_rate = 0.3
+        n_epoch = 500
+        n_hidden = 5
+        scores = evaluate_algorithm(dataset, back_propagation, n_folds, l_rate, n_epoch, n_hidden)
+        print('Scores: %s' % scores)
+        print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
 
 if __name__ == '__main__':
     unittest.main()
