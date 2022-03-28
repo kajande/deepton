2adc6d72300cd75af2bbe43862916b7c3eafd05d
Moctar Diallo <moctarjallo@gmail.com>
2022-03-28 12:19:06 +0000

    Implemented Dataset.__getitem|len|iter|__

--- test_main.py
+++ test_main.py
@@ -22,9 +22,32 @@ class TestDataset(unittest.TestCase):
         self.assertListEqual(dataset.data, self.data)
 
     def test_getitem(self):
-        pass
+        dataset = Dataset('example.csv')
+        elm = dataset[0]
+        self.assertListEqual(elm, self.data[0])
+
+    def test_iter(self):
+        dataset = Dataset('example.csv')
+        for elm in dataset:
+            pass
+        self.assertListEqual(elm, self.data[-1])
+
+    def test_len(self):
+        dataset = Dataset('example.csv')
+        self.assertEqual(len(dataset), len(self.data))
 
 class TestMain(unittest.TestCase):
+    def setUp(self) -> None:
+        # load and prepare data
+        filename = 'example.csv'
+        # filename = 'seeds_dataset.csv'
+        self.dataset = Dataset(filename)
+        for i in range(len(self.dataset[0])-1):
+            str_column_to_float(self.dataset, i)
+        # convert class column to integers
+        str_column_to_int(self.dataset, len(self.dataset[0])-1)
+
+
     def test_initialize_network(self):
         seed(1)
         network = initialize_network(2, 1, 2)
@@ -54,39 +77,20 @@ class TestMain(unittest.TestCase):
         # Test training backprop algorithm
         print("Testing train_network:")
         seed(1)
-        dataset = [[2.7810836,2.550537003,0],
-            [1.465489372,2.362125076,0],
-            [3.396561688,4.400293529,0],
-            [1.38807019,1.850220317,0],
-            [3.06407232,3.005305973,0],
-            [7.627531214,2.759262235,1],
-            [5.332441248,2.088626775,1],
-            [6.922596716,1.77106367,1],
-            [8.675418651,-0.242068655,1],
-            [7.673756466,3.508563011,1]]
-        n_inputs = len(dataset[0]) - 1
-        n_outputs = len(set([row[-1] for row in dataset]))
+        n_inputs = len(self.dataset[0]) - 1
+        n_outputs = len(set([row[-1] for row in self.dataset]))
         network = initialize_network(n_inputs, 2, n_outputs)
-        train_network(network, dataset, 0.5, 20, n_outputs)
+        train_network(network, self.dataset, 0.5, 20, n_outputs)
         for layer in network:
             print(layer)
 
     def test_predict(self):
         # Test making predictions with the network
         print("Testing predict:")
-        dataset = [[2.7810836,2.550537003,0],
-            [1.465489372,2.362125076,0],
-            [3.396561688,4.400293529,0],
-            [1.38807019,1.850220317,0],
-            [3.06407232,3.005305973,0],
-            [7.627531214,2.759262235,1],
-            [5.332441248,2.088626775,1],
-            [6.922596716,1.77106367,1],
-            [8.675418651,-0.242068655,1],
-            [7.673756466,3.508563011,1]]
+
         network = [[{'weights': [-1.482313569067226, 1.8308790073202204, 1.078381922048799]}, {'weights': [0.23244990332399884, 0.3621998343835864, 0.40289821191094327]}],
             [{'weights': [2.5001872433501404, 0.7887233511355132, -1.1026649757805829]}, {'weights': [-2.429350576245497, 0.8357651039198697, 1.0699217181280656]}]]
-        for row in dataset:
+        for row in self.dataset:
             prediction = predict(network, row)
             print('Expected=%d, Got=%d' % (row[-1], prediction))
 
@@ -94,22 +98,15 @@ class TestMain(unittest.TestCase):
         # Test Backprop on Seeds dataset
         print("Testing back_propagation algorithm:")
         seed(1)
-        # load and prepare data
-        filename = 'seeds_dataset.csv'
-        dataset = Dataset(filename)
-        for i in range(len(dataset[0])-1):
-            str_column_to_float(dataset, i)
-        # convert class column to integers
-        str_column_to_int(dataset, len(dataset[0])-1)
         # normalize input variables
-        minmax = dataset_minmax(dataset)
-        normalize_dataset(dataset, minmax)
+        minmax = dataset_minmax(self.dataset)
+        normalize_dataset(self.dataset, minmax)
         # evaluate algorithm
         n_folds = 5
         l_rate = 0.3
         n_epoch = 500
         n_hidden = 5
-        scores = evaluate_algorithm(dataset, back_propagation, n_folds, l_rate, n_epoch, n_hidden)
+        scores = evaluate_algorithm(self.dataset, back_propagation, n_folds, l_rate, n_epoch, n_hidden)
         print('Scores: %s' % scores)
         print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
 
