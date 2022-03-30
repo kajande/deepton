f63383414fdb130f463ea2c0876e82f5124bd8cf
Moctar Diallo <moctarjallo@gmail.com>
2022-03-30 12:10:42 +0000

    Refactored Network.__init__ vs Network.from_layers

--- deepton/model/network.py
+++ deepton/model/network.py
@@ -3,18 +3,21 @@ from random import random
 
 class Network:
     # Initialize a network
-    def __init__(self, n_inputs, n_hidden, n_outputs):
-        if not n_inputs or not n_hidden or not n_outputs:
-            self.layers = None
-        else:
-            self.layers = list()
-            hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
-            self.layers.append(hidden_layer)
-            output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
-            self.layers.append(output_layer)
-    
-    @classmethod
-    def from_layers(cls, layers):
-        self = cls(None, None, None)
+    def __init__(self, n_inputs=None, n_hidden=None, n_outputs=None, layers=None):
+        # if layers:
+        #     self.layers = layers
+        # else:
+        #     self.layers = self.from_layers(n_inputs, n_hidden, n_outputs)
+
+        if not layers:
+            layers = self.from_layers(n_inputs, n_hidden, n_outputs)
         self.layers = layers
-        return self
+
+    @staticmethod
+    def from_layers(n_inputs, n_hidden, n_outputs):
+        layers = list()
+        hidden_layer = [{'weights':[random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
+        layers.append(hidden_layer)
+        output_layer = [{'weights':[random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
+        layers.append(output_layer)
+        return layers
