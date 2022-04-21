19d347a1847ed4adc9fc5dcbce3587035403175d
Moctar Diallo <moctarjallo@gmail.com>
2022-04-21 02:24:36 +0000

    Introducing x_train, y_train instead of just train_set

--- deepton/agent/agent.py
+++ deepton/agent/agent.py
@@ -1,3 +1,4 @@
+from deepton.data.extractor import Col, ExtractList
 from deepton.model.neuron import Neuron
 from deepton.model.layer import Layer
 from deepton.model.network import Network
@@ -10,13 +11,16 @@ class Agent:
     def evaluate(self, algorithm, loader, *args, **kwargs):
         scores = list()
         for train_set, test_set, validation_set in loader:
-            n_inputs = len(train_set[0]) - 1
-            n_outputs = len(set([row[-1] for row in train_set]))
+            train_set = ExtractList(train_set)
+            x_train = train_set.col[:-1]
+            y_train = train_set.col[-1]
+            n_inputs = len(x_train[0])
+            n_outputs = len(set(y_train))
             network = Network(layers=[
                 Layer(neurons=[Neuron(n_inputs) for _ in range(kwargs['n_hidden'])]),
                 Layer(neurons=[Neuron(kwargs['n_hidden']) for _ in range(n_outputs)]),
             ])
-            algorithm.train(network, train_set)
+            algorithm.train(network, train_set.data)
             predicted = network.test(test_set)
             actual = [row[-1] for row in validation_set]
             score = self.metric(actual, predicted)
